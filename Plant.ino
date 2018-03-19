92f95f991844a7c4fd42257558064af0c2bfca33
Colin de Roos <colinderoos@hotmail.nl>
2018-03-19 23:08:29 +0100

    Remove unnecesarry OLED stuff and switch to ThingPulse library

--- Plant.ino
+++ Plant.ino
@@ -2,46 +2,52 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
+#include <SSD1306.h>
+
+#define I2C_SDA D5
+#define I2C_SCL D6
 
 Amux amux(A0, D2);
 Adafruit_BME280 Bme280;
+SSD1306 Oled(0x3c, I2C_SDA, I2C_SCL);
 
 void setup()   {                
   Serial.begin(9600);
   Wire.begin(D5, D6);
-  setupOLED();
   Bme280.begin();
-  printSensorValues();
+  Oled.init();
+  Oled.flipScreenVertically();
 }
 
-void loop() { 
-  delay(2000);
+void loop() {
   printSensorValues();
+  delay(2000);
 }
 
 void printSensorValues() {
-  clearDisplay();
+	Oled.clear();
   char res[24];
   char buff[5];
   dtostrf(Bme280.readTemperature(), 2, 1, buff);
   sprintf(res, "Temperature: %s C", buff);
-  writeText(res, 0, 0);
+  Oled.drawString(0, 0, res);
   
   dtostrf(Bme280.readHumidity(), 2, 1, buff);
   sprintf(res, "Humidity: %s%%", buff);
-  writeText(res, 0, 8);
+  Oled.drawString(0, 10, res);
   
   dtostrf(Bme280.readPressure(), 4, 1, buff);
   sprintf(res, "Pressure: %s hPa", buff);
-  writeText(res, 0, 16);
+  Oled.drawString(0, 20, res);
 
   dtostrf(percentifyAnalogInput(amux.getSoilMoisture()), 2, 1, buff);
   sprintf(res, "Soil moisture: %s%%", buff);
-  writeText(res, 0, 32);
+  Oled.drawString(0, 30, res);
 
   dtostrf(percentifyAnalogInput(amux.getLightIntensity()), 2, 1, buff);
   sprintf(res, "Light: %s%%", buff);
-  writeText(res, 0, 38);
+  Oled.drawString(0, 40, res);
+  Oled.display();
 }
 
 uint8_t percentifyAnalogInput(uint16_t value) {
