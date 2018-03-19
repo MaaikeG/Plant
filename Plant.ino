0dd64e24bed1419a9c1b851e1ff82150a5baa6bc
Colin de Roos <colinderoos@hotmail.nl>
2018-03-19 22:37:44 +0100

    remove unnecessary BME280 stuff
    Whether it is connected properly or not should be clear from the output of the sensors values

--- Plant.ino
+++ Plant.ino
@@ -1,13 +1,16 @@
 #include "Amux.h"
 #include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
 
 Amux amux(A0, D2);
+Adafruit_BME280 Bme280;
 
 void setup()   {                
   Serial.begin(9600);
   Wire.begin(D5, D6);
   setupOLED();
-  setupBME();
+  Bme280.begin();
   printSensorValues();
 }
 
@@ -20,15 +23,15 @@ void printSensorValues() {
   clearDisplay();
   char res[24];
   char buff[5];
-  dtostrf(getTemperature(), 2, 1, buff);
+  dtostrf(Bme280.readTemperature(), 2, 1, buff);
   sprintf(res, "Temperature: %s C", buff);
   writeText(res, 0, 0);
   
-  dtostrf(getHumidity(), 2, 1, buff);
+  dtostrf(Bme280.readHumidity(), 2, 1, buff);
   sprintf(res, "Humidity: %s%%", buff);
   writeText(res, 0, 8);
   
-  dtostrf(getPressure(), 4, 1, buff);
+  dtostrf(Bme280.readPressure(), 4, 1, buff);
   sprintf(res, "Pressure: %s hPa", buff);
   writeText(res, 0, 16);
 
