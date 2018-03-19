ad4342c9c9ab317071d5de97dd023f6b23e838b9
MaaikeG <maaikegalama@outlook.com>
2018-03-19 17:51:57 +0100

    Soil moisture/light sensor measurement

---
+++ AMUX.ino
@@ -0,0 +1,26 @@
+#define AMUX_SEL D2
+#define AMUX_OUT A0
+
+void setupAMUX() {
+  pinMode(AMUX_OUT, INPUT);
+  pinMode(AMUX_SEL, OUTPUT);
+}
+
+int getSoilMoisture() {
+  int val;
+  digitalWrite(AMUX_SEL, HIGH);
+  delay(100); // TODO: make clockwatch!
+  val = analogRead(AMUX_OUT);
+  // set back to low to avoid corrosion
+  digitalWrite(AMUX_SEL, LOW);
+  return (val / 1023.0) *100;
+}
+
+int getLightIntensity() {
+  digitalWrite(AMUX_SEL, LOW);
+  delay(100); // wait shortly for sensor value to stabilize (make clockwatch!)
+  int val = analogRead(AMUX_OUT);
+  delay(100); // wait shortly for sensor value to stabilize (make clockwatch!)
+  return (val /1023.0) *100;
+}
+
--- Plant.ino
+++ Plant.ino
@@ -5,13 +5,16 @@ void setup()   {
   Wire.begin(D5, D6);
   setupOLED();
   setupBME();
-  PrintSensorValues();
+  setupAMUX();
+  printSensorValues();
 }
 
 void loop() { 
+  delay(2000);
+  printSensorValues();
 }
 
-void PrintSensorValues() {
+void printSensorValues() {
   clearDisplay();
   char res[24];
   char buff[5];
@@ -26,5 +29,13 @@ void PrintSensorValues() {
   dtostrf(getPressure(), 4, 1, buff);
   sprintf(res, "Pressure: %s hPa", buff);
   writeText(res, 0, 16);
+
+  dtostrf(getSoilMoisture(), 2, 1, buff);
+  sprintf(res, "Soil moisture: %s%%", buff);
+  writeText(res, 0, 32);
+
+  dtostrf(getLightIntensity(), 2, 1, buff);
+  sprintf(res, "Light: %s%%", buff);
+  writeText(res, 0, 38);
 }
 
