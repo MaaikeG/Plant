42e8add3678b12f2c45c80cffbe94900c32d4d0e
MaaikeG <maaikegalama@outlook.com>
2018-03-21 12:30:31 +0100

    proj file

--- PlantNurseThing/ServoRegulator.cpp
+++ PlantNurseThing/ServoRegulator.cpp
@@ -11,12 +11,12 @@ ServoRegulator::ServoRegulator(uint8_t _servoPin) {
 void ServoRegulator::water() {
 	int pos;
 
-	for (pos = 0; pos <= 180; pos += 1) // goes from 0 degrees to 180 degrees 
+	for (pos = 0; pos <= 90; pos += 1) // goes from 0 degrees to 180 degrees 
 	{                                  // in steps of 1 degree 
 		servo.write(pos);              // tell servo to go to position in variable 'pos' 
 		delay(15);                       // waits 15ms for the servo to reach the position 
 	}
-	for (pos = 180; pos >= 0; pos -= 1)     // goes from 180 degrees to 0 degrees 
+	for (pos = 90; pos >= 0; pos -= 1)     // goes from 180 degrees to 0 degrees 
 	{
 		servo.write(pos);              // tell servo to go to position in variable 'pos' 
 		delay(15);                       // waits 15ms for the servo to reach the position 
