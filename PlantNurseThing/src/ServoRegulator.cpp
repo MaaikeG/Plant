0ee2d11dcfa3cfb7fccc015c0190b36362a0af2d
Colin de Roos <colinderoos@hotmail.nl>
2018-03-22 11:56:17 +0100

    remove trailing whitespace

--- PlantNurseThing/src/ServoRegulator.cpp
+++ PlantNurseThing/src/ServoRegulator.cpp
@@ -7,15 +7,15 @@ ServoRegulator::ServoRegulator(uint8_t _servoPin) {
 void ServoRegulator::water() {
 	int pos;
 
-	for (pos = 0; pos <= 90; pos += 1) // goes from 0 degrees to 180 degrees 
-	{                                  // in steps of 1 degree 
-		servo.write(pos);              // tell servo to go to position in variable 'pos' 
-		delay(15);                       // waits 15ms for the servo to reach the position 
+	for (pos = 0; pos <= 90; pos += 1) // goes from 0 degrees to 180 degrees
+	{                                  // in steps of 1 degree
+		servo.write(pos);              // tell servo to go to position in variable 'pos'
+		delay(15);                       // waits 15ms for the servo to reach the position
 	}
-	for (pos = 90; pos >= 0; pos -= 1)     // goes from 180 degrees to 0 degrees 
+	for (pos = 90; pos >= 0; pos -= 1)     // goes from 180 degrees to 0 degrees
 	{
-		servo.write(pos);              // tell servo to go to position in variable 'pos' 
-		delay(15);                       // waits 15ms for the servo to reach the position 
+		servo.write(pos);              // tell servo to go to position in variable 'pos'
+		delay(15);                       // waits 15ms for the servo to reach the position
 	}
 }
 
